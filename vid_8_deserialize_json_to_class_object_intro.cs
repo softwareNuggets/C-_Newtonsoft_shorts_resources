6478fcdd0b88f94ab95f6887cfc6ecc721c24fde
SoftwareNuggets <92217545+softwareNuggets@users.noreply.github.com>
2023-04-18 19:40:15 -0400

    intro to newtonsoft deserialization
    convert json text file to c# class object

---
+++ vid_8_deserialize_json_to_class_object_intro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace MyApp
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string fname = @"c:\temp\learning.JSON";
+
+            if (File.Exists(fname) == false)
+            {
+                Console.WriteLine(
+                     $"{fname} is not available");
+                return;
+            }
+
+            var json = 
+                File
+               .ReadAllText(fname);
+
+            Learn firstApp = 
+                JsonConvert
+                .DeserializeObject<Learn>(json);
+
+
+        }
+        
+        
+    }
+
+    public class Learn
+    {
+        public string a_string { get; set; }
+    }
+}
